e4fa5f21fa5a4cadfc0cc09e798934887d0fcca4
Christopher Cesar <chris.cesar@environment-agency.gov.uk>
2024-01-03 16:44:16 +0000

    Init

---
+++ R/EQETool.R
@@ -0,0 +1,14 @@
+# EQETool.R ####
+## Ecological Quality Extent (EQE) tool to quantify the impacts and recovery of environmental indices
+## A number of user-generated parameters are required to inform the spatial and temporal scales of pressures
+
+## user inputs:
+## use same symbology as the Cesar paper:
+
+# Ecological quality dimension:
+## Qmax is the maximum value of the index under reference conditions
+## Qmin is the minimum value that the EQR will reach as a result of the disturbance
+
+# Spatial dimension (Sp):
+# calculated as the surface area of the ‘footprint’ of the pressure as a
+# proportion of the assessment area
